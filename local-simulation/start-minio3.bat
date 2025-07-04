3c8fd294a231851b565a0a7324a236f92f832de3
zimin.zhang <zimin.zhang@bentley.com>
2025-07-04 10:30:26 +0800

    Update MinIO configuration for local simulation: correct data and console ports in README and batch files

--- local-simulation/start-minio3.bat
+++ local-simulation/start-minio3.bat
@@ -2,9 +2,9 @@
 set MINIO_ROOT_USER=minioadmin
 set MINIO_ROOT_PASSWORD=minioadmin
 minio.exe server ^
- http://127.0.0.1:9001/miniodata1 ^
- http://127.0.0.1:9002/miniodata2 ^
- http://127.0.0.1:9003/miniodata3 ^
- http://127.0.0.1:9004/miniodata4 ^
- --console-address :9013 --address :9003
+ http://127.0.0.1:9000/miniodata1 ^
+ http://127.0.0.1:9001/miniodata2 ^
+ http://127.0.0.1:9002/miniodata3 ^
+ http://127.0.0.1:9003/miniodata4 ^
+ --console-address :9012 --address :9002
 pause
